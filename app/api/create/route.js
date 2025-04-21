dd31727cdc194d0eb1cc4222102481f51ef4f1de
Mohit Singh <mscrick01@gmail.com>
2025-04-21 18:00:01 +0530

    third commit

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,12 +1,15 @@
-import { StreamChat } from "stream-chat"
-const api_key = "xjxsvqyuj8c9";
-const api_secret = "7wtpnufmdaakped8pumb85pf4p4randaa62qzpnn3xqxnt38x55xj68dm6bc7tfj";
-const user_id = "user_2vtWfdE08ZgHONSqtAYEhYGPadR";
+// app/api/token/route.ts
+import { NextResponse } from 'next/server';
+import { StreamChat } from 'stream-chat';
+
+const apiKey = process.env.STREAM_API_KEY!;
+const apiSecret = process.env.STREAM_API_SECRET!;
+const userId = 'user_2vtWfdE08ZgHONSqtAYEhYGPadR';
 
 export async function GET() {
-    const serverClient = StreamChat.getInstance(api_key, api_secret);
-    // Create User Token
-    const token = serverClient.createToken(user_id);
-    console.log(token)
-    return Response.json({ message: 'Hello World' })
-  }
+  const serverClient = StreamChat.getInstance(apiKey, apiSecret);
+
+  const token = serverClient.createToken(userId);
+
+  return NextResponse.json({ token });
+}
