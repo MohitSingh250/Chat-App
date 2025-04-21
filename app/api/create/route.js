49fbe315f5303f38a498208b1b313562c5054cda
Mohit Singh <mscrick01@gmail.com>
2025-04-21 18:09:06 +0530

    update

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -2,8 +2,8 @@
 import { NextResponse } from 'next/server';
 import { StreamChat } from 'stream-chat';
 
-const apiKey = process.env.STREAM_API_KEY!;
-const apiSecret = process.env.STREAM_API_SECRET!;
+const apiKey = process.env.STREAM_API_KEY;
+const apiSecret = process.env.STREAM_API_SECRET;
 const userId = 'user_2vtWfdE08ZgHONSqtAYEhYGPadR';
 
 export async function GET() {
